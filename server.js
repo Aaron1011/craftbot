84cc645c5ef8b5f5a9cbdb06875ec3793aa413f1
Aaron Hill <aa1ronham@gmail.com>
2014-01-28 19:53:18 -0500

    Fixed syntax

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.post('/gh_callback', function(res, request) {
   console.log("Payload : ", request.body.payload)
   console.log("Commit message: ", request.body.payload.commits[0].message)
-}
+});
 
 app.listen(ipAddress, port);
